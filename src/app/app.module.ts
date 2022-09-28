e7a75906279cc6fecf8f5776de490e968c8ff29d
Jangin.RASHEED <Bl@ugrana95830JR#>
2022-09-28 07:57:57 +0200

    Komponenten erstellt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GroupTableComponent } from './shared/group-table/group-table.component';
+import { MatchComponent } from './shared/match/match.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    GroupTableComponent,
+    MatchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  // private _dataApiUrl = 'http://localhost:8081/';
+  // private _newsApiUrl = 'https://newsdata.io/api/1/news?apikey=pub_107858411e9fea4c6d3e422f5cfd83713a8a7&q=champions%20league&language=' + this.newLanguage + '&category=sports';
+
+  constructor() {
+  }
+}
