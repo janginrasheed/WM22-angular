9510a4f1817e7b7f3b421e187072baa9becaa793
Jangin.RASHEED <Bl@ugrana95830JR#>
2022-09-29 16:38:00 +0200

    viel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {GroupTableComponent} from './shared/group-table/group-table.component';
-import {MatchComponent} from './shared/match/match.component';
+import {GroupTableComponent} from './components/shared/group-table/group-table.component';
+import {MatchComponent} from './components/shared/match/match.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
-import {NewsComponent} from './shared/news/news.component';
-import {LoginComponent} from './login/login.component';
+import {NewsComponent} from './components/shared/news/news.component';
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
 import {MatIconModule} from "@angular/material/icon";
 import {MatDialogModule} from "@angular/material/dialog";
-import {BotDialogComponent} from './shared/bot-dialog/bot-dialog.component';
+import {BotDialogComponent} from './components/shared/bot-dialog/bot-dialog.component';
+import {HomeComponent} from "./components/home/home.component";
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {LoginComponent} from "./components/login/login.component";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import {BotDialogComponent} from './shared/bot-dialog/bot-dialog.component';
     MatButtonModule,
     MatInputModule,
     MatIconModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {User} from "../types/user";
+import {User} from "../../types/user";
 
 @Component({
   selector: 'app-login',
